a11d74b5b3eedf4254bfd3c0bf43aa2c319ce263
tehpsalmist <bstewardcodes@gmail.com>
2019-06-08 14:00:16 -0400

    some cleanup and initial tailwind implementation

---
+++ ng-tailwind.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // Tailwind Paths
+  configJS: '/Users/tehpsalmist/Svelte/template/tailwind.config.js',
+  sourceCSS: '/Users/tehpsalmist/Svelte/template/tailwind.css',
+  outputCSS: '/Users/tehpsalmist/Svelte/template/public/global.css',
+  // Sass
+  sass: false,
+  // PurgeCSS Settings
+  purge: false,
+  keyframes: false,
+  fontFace: false,
+  rejected: false,
+  whitelist: [],
+  whitelistPatterns: [],
+  whitelistPatternsChildren: [],
+  extensions: ['.ts', '.html', '.js', '.svelte'],
+  extractors: [],
+  content: []
+}
--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,5 @@
 <script>
-	export let name;
+  export let name
 </script>
-
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
 
 <h1>Hello {name}!</h1>
